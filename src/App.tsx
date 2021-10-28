310dde47d92f713ce7a65f2543c20474f6452086
Francis Lagares <francis.lagares@gmail.com>
2021-10-28 21:08:00 +0200

    feat: create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './styles/index.css';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import Home from 'components/Home';
 
 const App = () => {
   return (
-    <div className='app'>
-      <h1>Hello React App</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path='/' component={Home} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
