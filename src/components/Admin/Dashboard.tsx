0184fddac750a711428fc636e09e2656c43268ea
Francis Lagares <francis.lagares@gmail.com>
2021-11-01 18:51:34 +0100

    feat: create Dashboard component

---
+++ src/components/Admin/Dashboard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Dashboard = (props: any) => {
+  console.log(props);
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      Dashboard <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />{' '}
+    </div>
+  );
+};
+
+export default Dashboard;
