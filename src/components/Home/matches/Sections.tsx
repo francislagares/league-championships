88b2b004a3eaba22e6c9ae611bbb9e95545288aa
Francis Lagares <francis.lagares@gmail.com>
2021-11-11 15:09:08 +0100

    feat: render matches list section

--- src/components/Home/matches/Sections.tsx
+++ src/components/Home/matches/Sections.tsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { getDocs } from 'firebase/firestore';
 import { Slide } from 'react-awesome-reveal';
 import { matchesCollection } from 'config/firebase';
+import MatchesBox from 'utils/matchesBox';
 
 const Sections = () => {
   const [matches, setMatches] = useState<IMatch[]>([]);
+  const slideSettings = {
+    bottom: true,
+  };
 
   useEffect(() => {
     if (!matches.length) {
@@ -20,9 +24,18 @@ const Sections = () => {
       getMatches();
     }
   }, [matches]);
-  console.log(matches);
 
-  return <div>Hello</div>;
+  const showMatches =
+    matches &&
+    matches.map(match => (
+      <Slide key={match.id} className='item' {...slideSettings} triggerOnce>
+        <div className='wrapper'>
+          <MatchesBox match={match} />
+        </div>
+      </Slide>
+    ));
+
+  return <div className='home_matches'>{showMatches}</div>;
 };
 
 export default Sections;
